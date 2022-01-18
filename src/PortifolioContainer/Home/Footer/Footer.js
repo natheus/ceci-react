df50ad0f909ab9b8a8dd0b4c7617e6465166c36b
Natheus <natheusmunes@gmail.com>
2022-01-17 22:01:35 -0300

    link footer

--- src/PortifolioContainer/Home/Footer/Footer.js
+++ src/PortifolioContainer/Home/Footer/Footer.js
@@ -4,7 +4,7 @@ import "./Footer.css"
 export default function Portifolio() {
   return (
     <footer className="container-fluid text-center bg-footer">
-      <p>Copyright © 2022 Natheus</p>
+      <p>Copyright © 2022 <a href="https://github.com/natheus/">Natheus</a></p>
     </footer>
   );
 }
